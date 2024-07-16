38ca5ca26951261c5001f053f144f8766dcb06de
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2024-07-16 18:29:53 -0400

    center header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  background: #282c34;
+  padding: 20px;
+  color: white;
+  text-align: center;
+`;
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <HeaderContainer>
       <h1>Climate Impact Tracker</h1>
-    </header>
+    </HeaderContainer>
   );
 };
 
